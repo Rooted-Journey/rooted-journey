f64d7605dbf391e82c675057a722b427505efd46
Christoph Lahner <clahner92@gmail.com>
2023-02-03 20:38:38 +0100

    Scroll earth just after meadow is offscreen

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
-onready var cam = $mainCam
-var camDelta = Vector2(0, 100)
+onready var cam = $MainCam
+onready var earth = $Earth
+onready var meadow = $Meadow
+const camDelta = Vector2(0, 100)
 
 func _ready():
 	pass
 	
 func _process(delta):
 	cam.position += delta * camDelta
+	
+	if not $Meadow/VisibilityNotifier2D.is_on_screen():
+		earth.position += delta * camDelta

fd0762352e6d526ef50538c57b219c05b53f94f0
Christoph Lahner <clahner92@gmail.com>
2023-02-03 20:55:40 +0100

    Fix position not found on colorrect

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,15 +1,11 @@
 extends Node2D
-
-onready var cam = $MainCam
-onready var earth = $Earth
-onready var meadow = $Meadow
 const camDelta = Vector2(0, 100)
 
 func _ready():
 	pass
 	
 func _process(delta):
-	cam.position += delta * camDelta
+	$MainCam.position += delta * camDelta
 	
 	if not $Meadow/VisibilityNotifier2D.is_on_screen():
-		earth.position += delta * camDelta
+		$Earth.rect_position  += delta * camDelta

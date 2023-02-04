c24542e394088eb7a662f4e1ff36b504fd4acd9c
Thomas Christlieb <thomaschristlieb@hotmail.com>
2023-02-04 16:46:44 +0100

    Fix things and moles

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -4,7 +4,7 @@ var startTime = 0
 var currentTime = 0
 var things = [preload("res://Assets/Graphics/lava.png"), preload("res://Assets/Graphics/uranium.png"), preload("res://Assets/Graphics/water.png")]
 var mole = preload("res://Assets/Graphics/mole.png")
-var aktMole
+var moleArr = []
 
 func _ready():
 	randomize()
@@ -20,9 +20,17 @@ func _process(delta):
 	currentTime = OS.get_system_time_msecs() - startTime
 	$HUD/Score.text = str(currentTime)
 	
+	# Move the moles
+	if moleArr.size() > 0:
+		for i in range(moleArr.size()):
+			if moleArr[i].flip_h == true:
+				moleArr[i].position.x += 0.05
+			else:
+				moleArr[i].position.x -= 0.05
+	
 func _on_MoleTimer_timeout():
 	if currentTime > 5000:
-		if randi() % 10 + 1 > 5:
+		if randi() % 10 + 1 >= 5:
 			var newMole = Sprite.new()
 			newMole.position.x = randi() % int(get_viewport().size.x)
 			newMole.position.y = randi() % int(get_viewport().size.y) + $MainCam.position.y
@@ -33,10 +41,8 @@ func _on_MoleTimer_timeout():
 			newMole.scale.y = 0.5
 			if randi() % 10 + 1 > 5:
 				newMole.flip_h = true
-			$TreeRoot.add_child(newMole)
-			if aktMole:
-				aktMole.queue_free()
-			aktMole = newMole
+			$TreeNode.add_child(newMole)
+			moleArr.append(newMole)
 		else:
 			var newThing = Sprite.new()
 			newThing.position.x = randi() % int(get_viewport().size.x)
@@ -46,11 +52,11 @@ func _on_MoleTimer_timeout():
 			newThing.z_index = 5
 			newThing.scale.x = 0.5
 			newThing.scale.y = 0.5
-			$TreeRoot.add_child(newThing)
+			$TreeNode.add_child(newThing)
 		
-		# Move last mole
-		if aktMole:
-			if aktMole.flip_h == true:
-				aktMole.position.x += 0.05
-			else:
-				aktMole.position.x -= 0.05
+		# Delete too many moles - 10 should be enough at all times
+		if moleArr.size() > 10:
+			for i in range(moleArr.size()-10):
+				moleArr[i].queue_free()
+				moleArr.remove(i)
+

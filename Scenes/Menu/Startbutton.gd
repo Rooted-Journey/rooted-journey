d62fb4298553b375374fa9b60386ab9e6bc82755
unknown <ingres@APT02-PF255HP5.SWG.cdc.root>
2023-02-04 10:00:53 +0100

    Refactor namings and project structure

---
+++ Scenes/Menu/Startbutton.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _on_Startbutton_pressed():
+	get_tree().change_scene("res://Scenes/Game/Game.tscn")
+	
--- scripts/Startbutton.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-func _on_Startbutton_pressed():
-	get_tree().change_scene("res://scenes/main.tscn")
-	

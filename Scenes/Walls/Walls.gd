f9b8fab391995083df8b7456bf7f4952491e9db3
unknown <ingres@APT02-PF255HP5.SWG.cdc.root>
2023-02-04 10:38:29 +0100

    Add right wall and adjust parameters

--- Scenes/Walls/Walls.gd
+++ Scenes/Walls/Walls.gd
@@ -1,19 +1,36 @@
 extends Node
 
 # TODO: Add as parameters and adjust
-const height = 10000
-const min_left = 200
-const max_left = 300
+export var width = 1250
+export var height = 10000
+export var min_left = 200
+export var max_left = 300
 const steps = 100
+const difficulty = 1.5 # multiplicator for how far the walls slide to the center
 
 func _ready():
-	var array = PoolVector2Array()
-	array.push_back(Vector2(0, 0)) # top left
-	array.push_back(Vector2(0, height)) # bottom left
+	var arrLeft = PoolVector2Array()
+	arrLeft.push_back(Vector2(0, 0)) # top left
+	arrLeft.push_back(Vector2(0, height)) # bottom left
+	
+	var arrRight = PoolVector2Array()
+	arrRight.push_back(Vector2(width, 0)) # top right
+	arrRight.push_back(Vector2(width, height)) # bottom right
+	
+	var min_left_eff = int(min_left * difficulty)
+	var max_left_eff = int(max_left * difficulty)
+	var min_right_eff = width - max_left_eff
+	var max_right_eff = width - min_left_eff
 	
 	for y in range(height, 0, -1 * steps):
-		var randX = randi() % (max_left - min_left) + min_left
-		array.push_back(Vector2(randX, y)) # Random offset
+		var randXLeft = randi() % (max_left_eff - min_left_eff) + min_left_eff
+		arrLeft.push_back(Vector2(randXLeft, y)) # Random offset
+		
+		var randXRight = randi() % (max_right_eff - min_right_eff) + min_right_eff
+		arrRight.push_back(Vector2(randXRight, y)) # Random offset
 	
-	$WallLeft/Polygon2D.polygon = array
-	$WallLeft/CollisionPolygon2D.polygon = array
+	$WallLeft/Polygon2D.polygon = arrLeft
+	$WallLeft/CollisionPolygon2D.polygon = arrLeft
+	
+	$WallRight/Polygon2D.polygon = arrRight
+	$WallRight/CollisionPolygon2D.polygon = arrRight

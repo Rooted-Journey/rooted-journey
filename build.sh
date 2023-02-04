e203aa70a3d8febe6c01ab5395348b18d404a0a0
Thomas Christlieb <thomaschristlieb@hotmail.com>
2023-02-04 10:06:47 +0100

    Prepare build pipeline

---
+++ build.sh
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf dist/
+mkdir -p dist
+
+echo 'Building for web ...'
+godot --no-window --export "HTML5" dist/index.html
+echo 'done.'
+
+echo 'Building for linux ...'
+godot --no-window --export 'Linux/X11' dist/rootedJourney.x86_64
+chmod a+x dist/rootedJourney.x86_64
+echo 'done.'
+
+echo 'Building for Windows ...'
+godot --no-window --export 'Windows Desktop' dist/rootedJourney.exe
+echo 'done.'

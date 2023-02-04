b28ee2f480d1d23f3bed8726e011be84eb681584
PyDario <dario@pichowsky.de>
2023-02-04 11:43:01 +0100

    Make up for camera offest

--- Scenes/TreeRoot/TreeRoot.gd
+++ Scenes/TreeRoot/TreeRoot.gd
@@ -3,7 +3,7 @@ extends Node2D
 const TREE_OFFSET = -18
 const MOVEMENT_MULTIPLIER = 0.2
 const ROOT_COLOR = Color(0.648438, 0.497726, 0.303955)
-const MEADOW_OFFSET = 64
+const CAMERA_OFFSET = 150
 
 var tree_bottom = Vector2(0,0)
 var root_position = Vector2(0,0)
@@ -11,13 +11,13 @@ var root_position = Vector2(0,0)
 func _enter_tree():
 	var tree = get_parent().get_node("Tree")
 	var mainCam = get_parent().get_node("MainCam")
-	tree_bottom = tree.position + (Vector2(TREE_OFFSET, (tree.texture.get_height() / 2) + MEADOW_OFFSET))
+	tree_bottom = tree.position + (Vector2(TREE_OFFSET, tree.texture.get_height() / 2))
 	root_position = Vector2(tree_bottom.x, mainCam.position.y)
 
 func _draw():
 	var mainCam = get_parent().get_node("MainCam")
 
-	root_position.y = mainCam.position.y
+	root_position.y = mainCam.position.y + CAMERA_OFFSET
 	draw_line(tree_bottom, root_position, ROOT_COLOR, 5.0)
 	
 func _process(delta):

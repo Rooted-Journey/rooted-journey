817f9ec94910981eeab68d36c33dbf99e286beba
Thomas Christlieb <thomaschristlieb@hotmail.com>
2023-02-04 14:51:11 +0100

    Prepare score

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+var startTime = 0
+var currentTime = 0
+
 func _ready():
-	pass
+	startTime = OS.get_unix_time()
 	
 func _process(delta):
 	$MainCam.position += delta * $MainCam.camDelta
 	
 	if not $Meadow/VisibilityNotifier2D.is_on_screen():
 		$Earth.rect_position  += delta * $MainCam.camDelta
+		
+	currentTime = OS.get_unix_time() - startTime
+	$HUD/Score.text = str(currentTime)

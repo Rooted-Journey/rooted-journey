af746dcdd95f3d9a8ba55ce9ea3b2c37f1a50182
Christoph Lahner <clahner92@gmail.com>
2023-02-04 17:25:39 +0100

    Enable control of root

--- Scenes/TreeRoot/TreeRoot.gd
+++ Scenes/TreeRoot/TreeRoot.gd
@@ -3,11 +3,24 @@ extends Line2D
 onready var cam = get_parent().get_node("MainCam")
 onready var tree = get_parent().get_node("Tree")
 onready var treebottom = tree.position.y + tree.texture.get_height() * tree.scale.y / 2
+onready var xCoord = tree.position.x;
+export var speedMultiplier = 100
 
 func _ready():
-	add_point(Vector2(cam.position.x, treebottom))
+	add_point(Vector2(xCoord, treebottom))
 
-func _process(_delta):
-	if cam.position.y > treebottom:
-		add_point(cam.position)
-		update()
+func _process(delta):
+	if cam.position.y < treebottom:
+		return
+
+	if(Input.is_action_pressed("ui_right")):
+		xCoord += delta * speedMultiplier
+	if(Input.is_action_pressed("ui_left")):
+		xCoord -= delta * speedMultiplier
+	
+	add_point(Vector2(xCoord, cam.position.y))
+	
+	while get_point_count() > 1000:
+		remove_point(0)
+	
+	update()

c6957b140da1e632046f85647acaf1c641e1f41c
Thomas Christlieb <thomaschristlieb@hotmail.com>
2023-02-05 11:56:54 +0100

    Prepare Try again button in credits. Move tree down

--- Scenes/Credits/Backbutton.gd
+++ Scenes/Credits/Backbutton.gd
@@ -1,7 +1,5 @@
 extends Button
 
 func _on_Backbutton_pressed():
-	var _unused = get_tree().change_scene("res://Scenes/Menu/Menu.tscn")
-
-func _ready():
-	grab_focus()
+	Globals.reset()
+	var _unused = get_tree().change_scene("res://Scenes/Game/Game.tscn")
--- Scenes/Credits/Credits.gd
+++ Scenes/Credits/Credits.gd
@@ -2,3 +2,8 @@ extends Node2D
 
 func _ready():
 	$CanvasLayer/ScoreLabel.text = str(Globals.score)
+	if Globals.score > 0:
+		$TryAgainButton.visible = true
+	else:
+		$TryAgainButton.visible = false
+	

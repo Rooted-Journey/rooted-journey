e1bc2f2a05b9f34c4c3b285bcbc264f4f7330699
PyDario <dario@pichowsky.de>
2023-02-05 14:54:45 +0100

    add game over sound

--- Scenes/Credits/Credits.gd
+++ Scenes/Credits/Credits.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 func _ready():
+	$AudioStreamPlayer.play()
 	$CanvasLayer/ScoreLabel.text = str(int(round(Globals.score)))
 	if Globals.score != 0:
 		$TryAgainButton.visible = true

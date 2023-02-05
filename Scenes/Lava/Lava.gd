c92c792ea8bd8db90049ccd22602ba3252524d16
PyDario <dario@pichowsky.de>
2023-02-05 12:51:19 +0100

    do not collide with object on invincibility

--- Scenes/Lava/Lava.gd
+++ Scenes/Lava/Lava.gd
@@ -1,10 +1,9 @@
-extends KinematicBody2D
+extends StaticBody2D
 
-func _physics_process(_delta):
+func _ready():
 	if Globals.invincibility_remaining <= 0:
 		collision_mask = 1
 		collision_layer = 1
 	else:
 		collision_mask = 2
 		collision_layer = 2
-	self.position.x += 5
--- Scenes/Mole/MoleLeft.gd
+++ Scenes/Mole/MoleLeft.gd
@@ -1,4 +1,10 @@
 extends KinematicBody2D
 
 func _physics_process(_delta):
+	if Globals.invincibility_remaining <= 0:
+		collision_mask = 1
+		collision_layer = 1
+	else:
+		collision_mask = 2
+		collision_layer = 2
 	self.position.x -= 5

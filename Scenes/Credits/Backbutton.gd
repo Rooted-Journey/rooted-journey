773a3e8031293150728f813a4ecc6932c2420b8b
Thomas Christlieb <thomaschristlieb@hotmail.com>
2023-02-05 12:11:41 +0100

    Focus Try again Button

--- Scenes/Credits/Backbutton.gd
+++ Scenes/Credits/Backbutton.gd
@@ -5,4 +5,5 @@ func _on_Backbutton_pressed():
 	var _unused = get_tree().change_scene("res://Scenes/Menu/Menu.tscn")
 	
 func _ready():
-	grab_focus()
+	if Globals.score == 0:
+		grab_focus()
--- Scenes/Credits/TryAgainButton.gd
+++ Scenes/Credits/TryAgainButton.gd
@@ -3,3 +3,7 @@ extends Button
 func _on_Backbutton_pressed():
 	Globals.reset()
 	var _unused = get_tree().change_scene("res://Scenes/Game/Game.tscn")
+
+func _ready():
+	if Globals.score != 0:
+		grab_focus()

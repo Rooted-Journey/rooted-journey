b8284b3b5b191eaa161b7e4c61a6cc9c6fa95835
Christoph Lahner <clahner92@gmail.com>
2023-02-05 10:20:39 +0100

    Add scene for global variables

--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -38,7 +38,8 @@ func _on_MoleTimer_timeout():
 			
 	# And now, make the camera faster
 	$MainCam.camDelta.y += 5
-	$HUD/Speed.text = "Speed: " + str($MainCam.camDelta.y - 100)
+	Globals.speed += 1
+	$HUD/Speed.text = "Speed: " + str(Globals.speed)
 
 func _on_TreeRoot_wall_collide():
 	var _resp = get_tree().change_scene("res://Scenes/Credits/Credits.tscn")
---
+++ Scenes/Globals.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var score = 0
+var speed = 1
+
+func reset():
+	score = 0
+	speed = 0
